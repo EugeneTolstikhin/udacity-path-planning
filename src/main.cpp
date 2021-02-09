d53ff40f94f7d125c80ba32f194164ac0e0ea9f8
IevgenTolstikhin <etolstikhin@gmail.com>
2021-02-09 17:53:05 +0100

    1st attempt of making vehicle drive [Status] Done [Topic] Udacity Self Driving Nanodegree program [Action] Path planning project [Subaction] Make vehicle drive

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
 
 // for convenience
 using nlohmann::json;
@@ -50,8 +51,24 @@ int main() {
     map_waypoints_dy.push_back(d_y);
   }
 
+  constexpr float SIMULATOR_FREQUENCY = 1.0 / 50; //0.02
+  constexpr float SPEED_LIMIT = 49.5; //mph
+  constexpr float LANE_WIDTH = 4.0; //m
+  constexpr size_t AMOUNT_OF_POINTS_FOREHAND = 3;
+  constexpr size_t AMOUNT_OF_POINTS_WHOLE_PATH = 50;
+  constexpr size_t TOO_CLOSE_POINTS = 30;
+  constexpr float SPEED_CHANGE = 0.224; //0.224mph = 10m/s
+
+  enum LANE
+  {
+    LEFT,
+    CENTRAL,
+    RIGHT
+  } lane = LANE::CENTRAL;
+  double ref_vel = SPEED_LIMIT;
+
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
-               &map_waypoints_dx,&map_waypoints_dy]
+               &map_waypoints_dx,&map_waypoints_dy, &lane, &ref_vel]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -98,6 +115,149 @@ int main() {
            *   sequentially every .02 seconds
            */
 
+          if (previous_path_x.size() != previous_path_y.size())
+          {
+            throw 1;
+          }
+
+          int prev_size = previous_path_x.size();
+
+          if (prev_size > 0)
+          {
+            car_s = end_path_s;
+          }
+
+          bool too_close = false;
+
+          //find ref_v to use
+          for (size_t i = 0; i < sensor_fusion.size(); ++i)
+          {
+            float d = sensor_fusion.at(i).at(6);
+
+            //Check the middle lane
+            if ((LANE_WIDTH / 2 + LANE_WIDTH * lane - LANE_WIDTH / 2) < d && d < (LANE_WIDTH / 2 + LANE_WIDTH * lane + LANE_WIDTH / 2))
+            {
+              double vx = sensor_fusion.at(i).at(3);
+              double vy = sensor_fusion.at(i).at(4);
+              double check_speed = std::sqrt(std::pow(vx, 2) + std::pow(vy, 2));
+              double check_car_s = sensor_fusion.at(i).at(5);
+
+              check_car_s += SIMULATOR_FREQUENCY * prev_size * check_speed;
+
+              if (check_car_s > car_s && check_car_s - car_s < TOO_CLOSE_POINTS)
+              {
+                too_close = true;
+
+                if (lane > LANE::LEFT)
+                {
+                  lane = LANE::LEFT;
+                }
+              }
+            }
+          }
+
+          if (too_close)
+          {
+            ref_vel -= SPEED_CHANGE;
+          }
+          else if (ref_vel < SPEED_LIMIT)
+          {
+            ref_vel += SPEED_CHANGE;
+          }
+
+          vector<double> ptsx;
+          vector<double> ptsy;
+
+          double ref_x = car_x;
+          double ref_y = car_y;
+          double ref_yaw = deg2rad(car_yaw);
+
+          if (prev_size < 2)
+          {
+            double prev_car_x = car_x - std::cos(car_yaw);
+            double prev_car_y = car_y - std::sin(car_yaw);
+
+            ptsx.push_back(prev_car_x);
+            ptsx.push_back(car_x);
+
+            ptsy.push_back(prev_car_y);
+            ptsy.push_back(car_y);
+          }
+          else
+          {
+            ref_x = previous_path_x.at(prev_size - 1);
+            ref_y = previous_path_y.at(prev_size - 1);
+
+            double ref_x_prev = previous_path_x.at(prev_size - 2);
+            double ref_y_prev = previous_path_y.at(prev_size - 2);
+
+            ref_yaw = std::atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+
+            ptsx.push_back(ref_x_prev);
+            ptsx.push_back(ref_x);
+
+            ptsy.push_back(ref_y_prev);
+            ptsy.push_back(ref_y);
+          }
+
+          //In Frenet add evenly 30m spaces points ahead of the starting reference
+          for (int i = 1; i <= AMOUNT_OF_POINTS_FOREHAND; ++i)
+          {
+            auto next_wp = getXY(car_s + i * TOO_CLOSE_POINTS, (LANE_WIDTH / 2 + LANE_WIDTH * lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+            ptsx.push_back(next_wp.at(0));
+            ptsy.push_back(next_wp.at(1));
+          }
+
+          if (ptsx.size() != ptsy.size())
+          {
+            throw 2;
+          }
+
+          for (size_t i = 0; i < ptsx.size(); ++i)
+          {
+            double shift_x = ptsx.at(i) - ref_x;
+            double shift_y = ptsy.at(i) - ref_y;
+
+            ptsx.at(i) = shift_x * std::cos(0 - ref_yaw) - shift_y * std::sin(0 - ref_yaw);
+            ptsy.at(i) = shift_x * std::sin(0 - ref_yaw) + shift_y * std::cos(0 - ref_yaw);
+          }
+
+          tk::spline s;
+          s.set_points(ptsx, ptsy);
+
+          for (size_t i = 0; i < previous_path_x.size(); ++i)
+          {
+            next_x_vals.push_back(previous_path_x.at(i));
+            next_y_vals.push_back(previous_path_y.at(i));
+          }
+
+          double target_x = static_cast<double>(TOO_CLOSE_POINTS);
+          double target_y = s(target_x);
+          double target_dist = std::sqrt(std::pow(target_x, 2) + std::pow(target_y, 2));
+
+          double x_add_on = 0.0;
+
+          for (size_t i = 1; i <= AMOUNT_OF_POINTS_WHOLE_PATH - previous_path_x.size(); ++i)
+          {
+            double N = target_dist / (SIMULATOR_FREQUENCY * ref_vel / (SPEED_CHANGE * 10));
+            double x_point = x_add_on + target_x / N;
+            double y_point = s(x_point);
+
+            x_add_on = x_point;
+
+            double x_ref = x_point;
+            double y_ref = y_point;
+
+            //rotate back it to normal after rotating it earlier
+            x_point = x_ref * std::cos(ref_yaw) - y_ref * std::sin(ref_yaw);
+            y_point = x_ref * std::sin(ref_yaw) + y_ref * std::cos(ref_yaw);
+
+            x_point += ref_x;
+            y_point += ref_y;
+
+            next_x_vals.push_back(x_point);
+            next_y_vals.push_back(y_point);
+          }
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
